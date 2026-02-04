ac128402c4111b922695c281f6e382f1d1011c9b
Claudia Silva <mkal72@hotmail.com>
2026-02-04 19:34:07 -0300

    Finaizamos a navBar e inicializamos o perfil

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import NavBar from "./components/NavBar/NavBar"
+import "./global.scss"
+
+function App() {
+  return (
+    <>
+    
+    <h2> Olá, boa noite! </h2>
+    </>
+  )
+}
+export default App
+
+
